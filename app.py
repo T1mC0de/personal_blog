d64f4a7e059c157f4d29283d7a1236ed254b154c
Timofei Sukhobok <tmsukhobok@edu.hse.ru>
2025-06-03 23:08:19 +0300

    next time make simple registration

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host="127.0.0.1")
